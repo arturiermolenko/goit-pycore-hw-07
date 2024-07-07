656f7974b39dcf024e53c0290304e89c80a81b8a
artur <arturiermolenko@gmail.com>
2024-07-07 13:02:40 +0200

    Task done

---
+++ error_decorator.py
@@ -0,0 +1,17 @@
+"""Decorator for handling errors that arise during the work of the bot"""
+import sys
+from typing import Callable
+
+
+def input_error(func: Callable) -> Callable:
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError as e:
+            print(e)
+        except IndexError as e:
+            return e
+        except KeyboardInterrupt:
+            print("\nInterrupted by user!!!")
+            sys.exit()
+    return inner
